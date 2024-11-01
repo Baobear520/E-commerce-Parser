ad6bf948b6f6bf1fd5f59bc50707eeb3fec12846
Dima <aldmikon@gmail.com>
2024-11-01 17:31:29 +0800

    Refactoring preparation process in the browser

--- parser/tasks/selenium_tasks.py
+++ parser/tasks/selenium_tasks.py
@@ -2,70 +2,92 @@ import queue
 import time
 
 from bs4 import BeautifulSoup
-from selenium.common import TimeoutException
+from selenium.common import TimeoutException,ElementClickInterceptedException, NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from urllib3.exceptions import MaxRetryError
 
+
+# top window
+# <div class="ui-dialog ui-widget ui-widget-content ui-corner-all ui-front bfx-wm-dialog ui-draggable ui-resizable" tabindex="-1" role="dialog" aria-describedby="bfx-wm-wrapper"
+# button
+# <a id="bfx-wm-close-button" class="bfx-wm-close" href="javascript:void(0)" aria-label="close"></a>
+
+# second window
+# <div class="modal show" id="welcome-email-modal" role="dialog" aria-modal="true" style="display: block;
+# button
+# <span class="consent-tracking-close svg-svg-22-cross-dims svg-svg-22-cross" id="consent-close"></span>
+
+
 def close_modal_windows(driver):
     # First modal window configuration
-    first_modal_selector = (By.CLASS_NAME, "bfx-wm-dialog")
-    first_close_button_selector = (By.ID, "bfx-wm-close-button")
+    top_modal_selector = (By.CLASS_NAME, "bfx-wm-dialog")
+    top_close_button_selector = (By.ID, "bfx-wm-close-button")
 
     # Second modal window configuration
-    second_modal_selector = (By.ID, "welcome-email-modal")
-    second_close_button_selector = (By.ID, "consent-close")
+    bottom_modal_selector = (By.ID, "welcome-email-modal")
+    bottom_close_button_selector = (By.ID, "consent-close")
 
     # Attempt to close the first modal
     try:
-        first_modal = WebDriverWait(driver, 15).until(
-            EC.visibility_of_element_located(first_modal_selector)
+        top_modal = WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(top_modal_selector)
         )
-        if first_modal:
+        if top_modal:
             close_button = WebDriverWait(driver, 5).until(
-                EC.element_to_be_clickable(first_close_button_selector)
+                EC.element_to_be_clickable(top_close_button_selector)
             )
             time.sleep(2)  # Adding a slight delay before clicking
             close_button.click()
-            print("First modal window closed.")
+            print("Top modal window closed.")
     except TimeoutException:
-        print("First modal window not found. Moving on...")
+        print("Top modal window not found. Moving on...")
+    except Exception as e:
+        print(f"Unexpected error occurred while handling modals: {e}")
+        driver.quit()
 
     # Attempt to close the second modal
     try:
-        second_modal = WebDriverWait(driver, 15).until(
-            EC.visibility_of_element_located(second_modal_selector)
+        bottom_modal = WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(bottom_modal_selector)
         )
-        if second_modal:
+        if bottom_modal:
             close_button = WebDriverWait(driver, 5).until(
-                EC.element_to_be_clickable(second_close_button_selector)
+                EC.element_to_be_clickable(bottom_close_button_selector)
             )
             time.sleep(2)  # Adding a slight delay before clicking
             close_button.click()
-            print("Second modal window closed.")
+            print("Bottom modal window closed.")
+            return True
     except TimeoutException:
-        print("Second modal window not found. Moving on...")
+        print("Bottom modal window not found. Moving on...")
+        return True
 
     except Exception as e:
         print(f"Unexpected error occurred while handling modals: {e}")
         driver.quit()
 
 
-    #first window
-    #<div class="ui-dialog ui-widget ui-widget-content ui-corner-all ui-front bfx-wm-dialog ui-draggable ui-resizable" tabindex="-1" role="dialog" aria-describedby="bfx-wm-wrapper"
-    #button
-    #<a id="bfx-wm-close-button" class="bfx-wm-close" href="javascript:void(0)" aria-label="close"></a>
-
-    #second window
-    #<div class="modal show" id="welcome-email-modal" role="dialog" aria-modal="true" style="display: block;
-    #button
-    #<span class="consent-tracking-close svg-svg-22-cross-dims svg-svg-22-cross" id="consent-close"></span>
-
 def select_section_from_dropdown_menu(driver):
-    element = driver.find_element(By.ID,"2534374302023689")
-    time.sleep(3)
-    element.click()
+    while True:
+        try:
+            element = WebDriverWait(driver, 5).until(
+                EC.element_to_be_clickable((By.ID,"2534374302023689"))
+            )
+            element.click()
+            driver.implicitly_wait(3)
+            print(driver.current_url)
+            break
+
+        except ElementClickInterceptedException as e:
+            print("Encountered modal windows.")
+            modals_passed = close_modal_windows(driver)
+            if not modals_passed:
+                print("Could not close all modal windows. Exiting.")
+                break  # Exit the loop if modals cannot be closed
+
+
 
 
 def scroll_to_pagination(driver):
@@ -101,11 +123,10 @@ def scrape_product_links(driver):
     """Извлекаем ссылки на продукты с текущей страницы."""
 
     # После завершения прокрутки извлекаем все ссылки
-    product_urls = queue.Queue()
     product_elements = driver.find_elements(By.CSS_SELECTOR, "a.thumb-link")
     print(f"Got {len(product_elements)} items from the page")
-    [product_urls.put(link.get_attribute('href')) for link in product_elements]
-    return product_urls
+    return list([link.get_attribute('href') for link in product_elements])
+
 
 
 
